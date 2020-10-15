0dfcd16b0ae4b3f37179a52e54f9b97bcce56183
Dereck Sherrer <derecksherrer@yahoo.ca>
2020-10-15 00:21:04 -0400

    New Grid button works but need to fix sizing of new cells.

--- scripts.js
+++ scripts.js
@@ -1,22 +1,32 @@
-function createColumn(){    
-    for (let i=0; i<256; i++) {
+function createColumn(size) {
+    for (let i = 0; i < size; i++) {
         let newDiv = document.createElement("div");
         newDiv.classList.add("grid");
-        document.getElementById("main-container").appendChild(newDiv);
-        
+        document.getElementById("grid-container").appendChild(newDiv);
+
     };
 };
 
-function drawing(){
-    let draw = document.getElementById("main-container");
-    draw.addEventListener("mouseout", function(event){
+function drawing() {
+    let draw = document.getElementById("grid-container");
+    draw.addEventListener("mouseout", function (event) {
         event.target.style.backgroundColor = "black";
     });
 };
 
-function setGrid(){
+function setGrid() {
+    let size = prompt("Choose a new grid size");
+    while (size > 100) {
+        size = prompt("Please choose a number between 1 and 100");
+    };
+    let clearGrid = document.getElementById("grid-container");
+    clearGrid.remove();
+    let newGrid = document.createElement("div")
+    newGrid.id = "grid-container";
+    document.getElementById("main-container").appendChild(newGrid);
 
+    createColumn(size * size);
 };
 
-createColumn();
+createColumn(256);
 drawing();
