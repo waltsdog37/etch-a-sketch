0f90cbd4617da074c48ce520ad7b85fb1b846929
Dereck Sherrer <derecksherrer@yahoo.ca>
2020-10-29 19:36:33 -0400

    Added styling

--- scripts.js
+++ scripts.js
@@ -21,7 +21,7 @@ function createRow(size, i) {
 
 function drawing(color) {
     let random = document.getElementById("random-color");
-    random.addEventListener("click", randomColor);
+    random.addEventListener("click", newColor);
 
     let draw = document.getElementById("grid-container");
     console.log("drawing");
@@ -31,15 +31,18 @@ function drawing(color) {
     });
 }
 
-function randomColor() {
-    let color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+function newColor(element) {
+    let color = element.id;
+    if (color == "random-color") {
+        color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    }
     console.log(color);
     drawing(color);
 }
 
 function setGrid() {
     let size = prompt("Choose a new grid size");
-    while (size > 100) {
+    while (size > 100 || size < 1 || isNaN(size)) {
         size = prompt("Please choose a number between 1 and 100");
     };
     let clearGrid = document.getElementById("grid-container");
